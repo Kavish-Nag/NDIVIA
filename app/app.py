a3cce989b721427bb15a2ff58ab164869cd2f3c9
RishitP <rishitprabhu.personal@gmail.com>
2025-03-08 10:10:29 +0530

    Updated app.py

--- app/app.py
+++ app/app.py
@@ -3,7 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def login():
     return render_template('login.html')
+
+@app.route("/signup")
+def signup():
+        return render_template('signup.html')
 
 app.run(host='127.0.0.1', port=5000, debug=True)
